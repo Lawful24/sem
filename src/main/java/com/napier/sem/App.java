5cd748ae130bddac10b463ffc543a759cee7f392
Lawful <tarkanyilacko@gmail.com>
2022-02-02 18:59:58 +0000

    Added a MongoDB test to App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,32 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Boo Yah!");
+        // Connect to MongoDB on local system - we're using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        // Create a document to store
+        Document doc = new Document("name", "Laszlo Tarkanyi")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2022")
+                .append("result", new Document("CW", 100).append("EX", 100));
+
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
